8b45f0e0e43b1333717805821633d02e9ab42c25
wahyu93 <wahyu.wahyudi1005@gmail.com>
2025-07-11 23:08:34 +0800

    productcategory crud [done]

---
+++ app/Filament/Resources/ProductCategoryResource.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductCategoryResource\Pages;
+use App\Filament\Resources\ProductCategoryResource\RelationManagers;
+use App\Models\ProductCategory;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
+
+class ProductCategoryResource extends Resource
+{
+    protected static ?string $model = ProductCategory::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    protected static ?string $label = 'Manajemen Kategori Product';
+
+    protected static ?string $navigationGroup = 'Manajemen Menu';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->label('Toko')
+                    ->required()
+                    ->hidden(fn() => Auth::user()->role === 'store')
+                    ->relationship('user','name'),
+                Forms\Components\TextInput::make('name')
+                    ->label('Kategori')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Nama Toko')
+                    ->hidden(fn() => Auth::user()->role === 'store')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Kategori')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('user')
+                    ->relationship('user','name')
+                    ->hidden(fn() => Auth::user()->role === 'store'),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageProductCategories::route('/'),
+        ];
+    }
+
+    // static function untuk mencegah user melihat inputan user lain
+    public static function getEloquentQuery(): Builder
+    {
+        $user = Auth::user();
+
+        if($user->role === 'admin'){
+            return parent::getEloquentQuery();
+        }
+
+        return parent::getEloquentQuery()->where('user_id', $user->id);
+
+
+    }
+}
--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -4,12 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class ProductCategory extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'name', 'slug'];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function($model){
+            if(Auth::user()->role === 'store'){
+                $model->user_id =Auth::user()->id;
+            };
+
+            $model->slug = Str::slug($model->name);
+        });
+
+        static::updating(function($model){
+            if(Auth::user()->role === 'store'){
+                $model->user_id =Auth::user()->id;
+            };
+
+            $model->slug = Str::slug($model->name);
+        });
+
+    }
 
     public function user()
     {
